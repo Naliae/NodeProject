bc4e89cec0d7bbdc577ec8d4880a40297c42b137
Naliae <magali.heraud@hotmail.fr>
2016-03-03 23:48:01 +0100

    replace files

--- routes/songs.js
+++ routes/songs.js
@@ -1,120 +1,186 @@
 var express = require('express');
+var _ = require('lodash');
 var router = express.Router();
 var SongService = require('../services/songs');
-var _ = require('lodash');
+
+var verifyIsAdmin = function(req, res, next) {
+    if (req.isAuthenticated() && req.user.username === 'admin') {
+        return next();
+    }
+    else {
+        res.status(403).send({err: 'Current user can not access to this operation'});
+    }
+};
 
 router.get('/', function(req, res) {
-if (req.accepts('text/html') || req.accepts('application/json')) {
-  SongService.findAll()
-   .then(function(songs) {
-     if(!songs){
-     res.status(404).send({err: 'Error'});
-     return;
-     }
-     if (req.accepts('text/html')) {
-     return res.render('songs', {songs: songs});
-     }
-     if (req.accepts('application/json')) {
-     return res.status(200).send(songs);
-     }
-   });
-}
-else {
-res.status(406).send({err: 'Not valid type for asked ressource'});
-}
+    if (req.accepts('text/html') || req.accepts('application/json')) {
+        SongService.find(req.query || {})
+            .then(function(songs) {
+                if (req.accepts('text/html')) {
+                    return res.render('songs', {songs: songs});
+                }
+                if (req.accepts('application/json')) {
+                    res.status(200).send(songs);
+                }
+            })
+        ;
+    }
+    else {
+        res.status(406).send({err: 'Not valid type for asked ressource'});
+    }
 });
 
 router.get('/add', function(req, res) {
- if (req.accepts('text/html') || req.accepts('application/json')) {
-   SongService.findAll()
-    .then(function(form) {
-      if(!form){
-      res.status(404).send({err: 'Error'});
-      return;
-      }
-      if (req.accepts('text/html')) {
-      return res.render('form', {form: form});
-      }
-      if (req.accepts('application/json')) {
-      return res.status(200).send(form);
-      }
-    });
- }
- else {
- res.status(406).send({err: 'Not valid type for asked ressource'});
- }
+    var song = (req.session.song) ? req.session.song : {};
+    var err = (req.session.err) ? req.session.err : null;
+    if (req.accepts('text/html')) {
+        req.session.song = null;
+        req.session.err = null;
+        return res.render('newSong', {song: song, err: err});
+    }
+    else {
+        res.status(406).send({err: 'Not valid type for asked ressource'});
+    }
 });
 
 router.get('/:id', function(req, res) {
- if (req.accepts('text/html') || req.accepts('application/json')) {
-   SongService.findOneByQuery({_id: req.params.id})
-   .then(function(song) {
-     if (!song) {
-       res.status(404).send({err: 'No song found with id' + req.params.id});
-       return;
-     }
-     if (req.accepts('text/html')) {
-       return res.render('song', {song: song});
-     }
-     if (req.accepts('application/json')) {
-       return res.status(200).send(song);
-     }
-   });
- }
-  else {
-   res.status(406).send({err: 'Not valid type for asked ressource'});
- }
+    if (req.accepts('text/html') || req.accepts('application/json')) {
+        SongService.findOneByQuery({_id: req.params.id})
+            .then(function(song) {
+                if (!song) {
+                    res.status(404).send({err: 'No song found with id' + req.params.id});
+                    return;
+                }
+                if (req.accepts('text/html')) {
+                    return res.render('song', {song: song});
+                }
+                if (req.accepts('application/json')) {
+                    return res.send(200, song);
+                }
+            })
+            .catch(function(err) {
+                console.log(err);
+                res.status(500).send(err);
+            })
+        ;
+    }
+    else {
+        res.status(406).send({err: 'Not valid type for asked ressource'});
+    }
 });
 
-router.get('/songs', function(req, res) {
-  SongService.search(req.query || {})
-    .then(function(songs) {
-      res.status(200).send(songs);
-    });
+router.get('/artist/:artist', function(req, res) {
+    SongService.find({artist: {$regex: req.params.artist, $options: 'i'}})
+        .then(function(songs) {
+            res.status(200).send(songs);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.status(500).send(err);
+        })
+    ;
 });
 
-router.post('/', function(req, res) {
- if (!_.has(req.body, 'title') || !_.has(req.body, 'album') || !_.has(req.body, 'artist')) {
-   return res.status(400).send('title, album and artist are mandatory');
- }
- SongService.create(req.body)
-   .then(function(resultat){
-     if (req.accepts('text/html')) {
-       return res.redirect('/songs/' + resultat._id);
-     }
-     if (req.accepts('application/json')) {
-       res.status(201).send(resultat);
-     }
-   })
-   .catch(function(err) {
-       res.status(500).send(err);
-   });
+var songBodyVerification = function(req, res, next) {
+    var attributes = _.keys(req.body);
+    var mandatoryAttributes = ['title', 'album', 'artist'];
+    var missingAttributes = _.difference(mandatoryAttributes, attributes);
+    if (missingAttributes.length) {
+        res.status(400).send({err: missingAttributes.toString()});
+    }
+    else {
+        if (req.body.title && req.body.album && req.body.artist) {
+            next();
+        }
+        else {
+            var error = mandatoryAttributes.toString() + ' are mandatory';
+            if (req.accepts('text/html')) {
+                req.session.err = error;
+                req.session.song = req.body;
+                res.redirect('/songs/add');
+            }
+            else {
+                res.status(400).send({err: error});
+            }
+        }
+    }
+};
+
+router.post('/', verifyIsAdmin, songBodyVerification, function(req, res) {
+    SongService.create(req.body)
+        .then(function(song) {
+            if (req.accepts('text/html')) {
+                return res.redirect('/songs/' + song._id);
+            }
+            if (req.accepts('application/json')) {
+                return res.status(201).send(song);
+            }
+        })
+        .catch(function(err) {
+            res.status(500).send(err);
+        })
+    ;
 });
 
-router.put('/:id', function(req, res) {
- SongService.update(req.params.id, req.body)
-   .then(function(resultat) {
-     res.status(200).send(resultat);
-    })
-    .catch(function(err){
-      res.status(500).send(err);
-    })
- ;
+router.delete('/', verifyIsAdmin, function(req, res) {
+    SongService.deleteAll()
+        .then(function(songs) {
+            res.status(200).send(songs);
+        })
+        .catch(function(err) {
+            res.status(500).send(err);
+        })
+    ;
 });
 
-router.delete('/:id', function(req, res) {
- SongService.remove({_id: req.params.id})
-   .then(function(resultat) {
-     if (!resultat) {
-       res.status(404).send({err: 'No song found with id' + req.params.id});
-       return;  // important !!
-     }
-     res.status(204).send();
-    })
-    .catch(function(err){
-      res.status(500).send(err);
-    })
- ;
+router.get('/edit/:id', verifyIsAdmin, function(req, res) {
+    var song = (req.session.song) ? req.session.song : {};
+    var err = (req.session.err) ? req.session.err : null;
+    if (req.accepts('text/html')) {
+        SongService.findOneByQuery({_id: req.params.id})
+            .then(function(song) {
+                if (!song) {
+                    res.status(404).send({err: 'No song found with id' + req.params.id});
+                    return;
+                }
+                return res.render('editSong', {song: song, err: err});
+            })
+        ;
+    }
+    else {
+        res.status(406).send({err: 'Not valid type for asked ressource'});
+    }
+});
+
+router.put('/:id', verifyIsAdmin, function(req, res) {
+    SongService.updateSongById(req.params.id, req.body)
+        .then(function (song) {
+            if (!song) {
+                res.status(404).send({err: 'No song found with id' + req.params.id});
+                return;
+            }
+            if (req.accepts('text/html')) {
+                return res.redirect('/songs/' + song._id);
+            }
+            if (req.accepts('application/json')) {
+                res.status(200).send(song);
+            }
+        })
+        .catch(function (err) {
+            res.status(500).send(err);
+        })
+    ;
+});
+
+router.delete('/:id', verifyIsAdmin, function(req, res) {
+    SongService.removeAsync({_id: req.params.id})
+        .then(function() {
+            res.status(204);
+        })
+        .catch(function(err) {
+            res.status(500).send(err);
+        })
+    ;
 });
 
 module.exports = router;
--- services/songs.js
+++ services/songs.js
@@ -1,28 +1,24 @@
-'use strict';
-
+'use strict'
 var Promise = require('bluebird');
 var Songs = Promise.promisifyAll(require('../database/songs'));
 
-exports.findAll = function() {
-   return Songs.findAsync();
+exports.find = function(query) {
+    return Songs.findAsync(query);
 };
 
 exports.findOneByQuery = function(query) {
-   return Songs.findOneAsync(query);
+    return Songs.findOneAsync(query);
 };
 
 exports.create = function(song) {
-   return Songs.createAsync(song);
+    return Songs.createAsync(song);
 };
 
-exports.update = function(id, objectToUpdate) {
-   return Songs.findOneAndUpdateAsync({_id: id}, objectToUpdate, {new: true});
+exports.deleteAll = function() {
+    return Songs.removeAsync();
 };
 
-exports.remove = function(id) {
-  return Songs.findOneAndRemoveAsync({_id:id} );
+exports.updateSongById = function(songId, songToUpdate) {
+    // return Songs.updateAsync({_id: songId}, songToUpdate); // updates but doesn't return updated document
+    return Songs.findOneAndUpdateAsync({_id: songId}, songToUpdate, {new: true}); // https://github.com/Automattic/mongoose/issues/2756
 };
-
-exports.search = function(ObjectToSearch) {
-  return Songs.findAsync(ObjectToSearch)
-}
